5531d8ff71b3efe3975ea97c1796e99a66475453
NotAlexNoyle <alexnoyle@icloud.com>
2025-09-23 21:50:41 -0400

    Update gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ rootProject.name = "MiniPlaceholders-LuckPerms"
 dependencyResolutionManagement {
     repositories {
         maven("https://repo.papermc.io/repository/maven-public/")
-        //maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
@@ -13,18 +12,16 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
-        maven("https://maven.fabricmc.net/")
         maven("https://maven.quiltmc.org/repository/release/")
     }
 }
 
-arrayOf("common", "paper", "velocity", "sponge", "fabric").forEach {
+arrayOf("common", "paper", "velocity", "sponge").forEach {
     include("luckperms-expansion-$it")
     project(":luckperms-expansion-$it").projectDir = file(it)
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-    id("fabric-loom") version "1.4.4"
     id("org.spongepowered.gradle.plugin") version "2.2.0"
 }
